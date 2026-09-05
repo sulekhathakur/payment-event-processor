d50e3a0cb1edc11c7ad1ec8262e6af995ec7ae6c
sulekhathakur <sulekhathakur787@gmail.com>
2026-09-05 16:09:18 +0530

    chore: initialize Spring Boot project

---
+++ src/test/java/com/paymentprocessor/PaymentprocessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentprocessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
